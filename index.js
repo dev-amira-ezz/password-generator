8308e017ccb589e133591f9a0a446634d1c0f58b
Amira Ezz <aezz4007@gmail.com>
2025-04-05 10:58:17 +0200

    Add password generation functionality

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7",
 "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-",
 "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+
+let password1El = document.getElementById("password1-el");
+let password2El = document.getElementById("password2-el");
+let password1 = [];
+let password2 = [];
+
+function generatePassword() {
+    for(let i=0; i<15;i++) {
+        password1.push(characters[Math.floor(Math.random() * characters.length)]);
+        password2.push(characters[Math.floor(Math.random() * characters.length)]);
+    }
+    password1El.textContent += password1.join("");
+    password2El.textContent += password2.join("");
+}
+
